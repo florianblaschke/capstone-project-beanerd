e925af31239ae61c5e808032d3ff0a3412646de6
Florian Blaschke <florianblaschke@web.de>
2023-07-28 13:00:21 +0200

    fallback font

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
 import GlobalStyle from "@/styles";
 import Head from "next/head";
-import { styled } from "styled-components";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700"],
+  fallback: ["system-ui"],
 });
 
 export default function App({ Component, pageProps }) {
