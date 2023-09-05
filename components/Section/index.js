09ca8841d09d99358649a04b8696693874d0f5fa
Florian Blaschke <florianblaschke@web.de>
2023-09-05 12:18:30 +0200

    added new overlay on startpage

---
+++ components/Section/index.js
@@ -0,0 +1,129 @@
+import RoastCard from "../RoastCard";
+import {
+  StyledList,
+  StyledItem,
+  StyledSectionWrapper,
+  StyledSectionOver,
+  StyledSectionUnder,
+  StyledHeadingSection,
+  StyledLinkSmooth,
+  StyledDivButtonWrapperVariant,
+} from "@/lib/styled-components";
+
+export default function Sections({
+  arabica,
+  robusta,
+  newIn,
+  topRated,
+  favorites,
+  session,
+}) {
+  return (
+    <>
+      <StyledDivButtonWrapperVariant>
+        <StyledLinkSmooth href={"#topRated"}>Top bewertet</StyledLinkSmooth>
+        <StyledLinkSmooth href={"#newIn"}>Neu</StyledLinkSmooth>
+        <StyledLinkSmooth href={"#arabica"}>100% Arabica</StyledLinkSmooth>
+        <StyledLinkSmooth href={"#robusta"}>100% Robusta</StyledLinkSmooth>
+      </StyledDivButtonWrapperVariant>
+      <StyledSectionWrapper>
+        <StyledSectionUnder>
+          <StyledHeadingSection id="topRated">
+            Top bewertet
+          </StyledHeadingSection>
+          <StyledList>
+            {topRated.map((roast, i) =>
+              i >= 3 ? null : (
+                <StyledItem key={roast._id}>
+                  <RoastCard
+                    id={roast._id}
+                    isFavorite={
+                      session &&
+                      favorites.roasts.some(
+                        (roastEntry) => roastEntry._id === roast._id
+                      )
+                    }
+                    name={roast.name}
+                    roaster={roast.roaster}
+                    score={roast.score.map(({ rating }) => rating)}
+                  />
+                </StyledItem>
+              )
+            )}
+          </StyledList>
+        </StyledSectionUnder>
+      </StyledSectionWrapper>
+      <StyledSectionOver>
+        <StyledHeadingSection id="newIn">Neu hinzugefügt</StyledHeadingSection>
+        <StyledList>
+          {newIn.map((roast, i) =>
+            i >= 3 ? null : (
+              <StyledItem key={roast._id}>
+                <RoastCard
+                  id={roast._id}
+                  isFavorite={
+                    session &&
+                    favorites.roasts.some(
+                      (roastEntry) => roastEntry._id === roast._id
+                    )
+                  }
+                  name={roast.name}
+                  roaster={roast.roaster}
+                  score={roast.score.map(({ rating }) => rating)}
+                />
+              </StyledItem>
+            )
+          )}
+        </StyledList>
+      </StyledSectionOver>
+      <StyledSectionWrapper>
+        <StyledSectionUnder>
+          <StyledHeadingSection id="arabica">100% Arabica</StyledHeadingSection>
+          <StyledList>
+            {arabica.map((roast, i) =>
+              i >= 3 ? null : (
+                <StyledItem key={roast._id}>
+                  <RoastCard
+                    id={roast._id}
+                    isFavorite={
+                      session &&
+                      favorites.roasts.some(
+                        (roastEntry) => roastEntry._id === roast._id
+                      )
+                    }
+                    name={roast.name}
+                    roaster={roast.roaster}
+                    score={roast.score.map(({ rating }) => rating)}
+                  />
+                </StyledItem>
+              )
+            )}
+          </StyledList>
+        </StyledSectionUnder>
+      </StyledSectionWrapper>
+      <StyledSectionOver>
+        <StyledHeadingSection id="robusta">100% Robusta</StyledHeadingSection>
+        <StyledList>
+          {robusta.map((roast, i) =>
+            i >= 3 ? null : (
+              <StyledItem key={roast._id}>
+                <RoastCard
+                  id={roast._id}
+                  isFavorite={
+                    session &&
+                    favorites.roasts.some(
+                      (roastEntry) => roastEntry._id === roast._id
+                    )
+                  }
+                  name={roast.name}
+                  roaster={roast.roaster}
+                  score={roast.score.map(({ rating }) => rating)}
+                />
+              </StyledItem>
+            )
+          )}
+        </StyledList>
+      </StyledSectionOver>
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import RoastCard from "@/components/RoastCard";
-import { StyledList, StyledItem } from "@/lib/styled-components";
 import { useSession } from "next-auth/react";
 import useSWR from "swr";
+import Sections from "@/components/Section";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -15,6 +14,30 @@ export default function Home() {
   );
   if (isLoading || favoritesLoading) return <h1>... is Loading</h1>;
 
+  const roastsWithReducedScore = data.map((roast) => {
+    if (roast.score.length === 0) return { ...roast, reducedScore: 0 };
+    const reducedScore =
+      roast.score
+        .map(({ rating }) => rating)
+        .reduce((acc, curr) => acc + curr, 0) / roast.score.length;
+    return { ...roast, reducedScore: reducedScore };
+  });
+
+  const arabica = data.filter(({ arabica }) => arabica === 100);
+  const robusta = data.filter(({ robusta }) => robusta === 100);
+  const topRated = roastsWithReducedScore.sort(
+    (a, b) => b.reducedScore - a.reducedScore
+  );
+  const newIn = data.toReversed();
+
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   return (
     <>
       <Head>
@@ -23,24 +46,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <StyledList>
-        {data.map((roast) => (
-          <StyledItem key={roast._id}>
-            <RoastCard
-              id={roast._id}
-              isFavorite={
-                session &&
-                favorites.roasts.some(
-                  (roastEntry) => roastEntry._id === roast._id
-                )
-              }
-              name={roast.name}
-              roaster={roast.roaster}
-              score={roast.score.map(({ rating }) => rating)}
-            />
-          </StyledItem>
-        ))}
-      </StyledList>
+      <Sections
+        arabica={shuffle(arabica)}
+        robusta={shuffle(robusta)}
+        newIn={newIn}
+        topRated={topRated}
+        favorites={favorites}
+        session={session}
+      />
     </>
   );
 }
